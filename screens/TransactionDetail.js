88612f7333bc7a724696931c32718ee99a2b3fb7
Shanksarc21 <149334464+Shanksarc21@users.noreply.github.com>
2024-07-21 13:07:09 -0400

    FinancialApp_Lab03

--- screens/TransactionDetail.js
+++ screens/TransactionDetail.js
@@ -3,9 +3,9 @@ import { View, Text, StyleSheet } from 'react-native';
 
 const TransactionDetail = ({ route }) => {
   const { transaction } = route.params;
+
   return (
     <View style={styles.container}>
-      <Text style={styles.header}>Transaction Detail</Text>
       <Text style={styles.text}>Name: {transaction.name}</Text>
       <Text style={styles.text}>Amount: {transaction.amount}</Text>
       <Text style={styles.text}>Date: {transaction.date}</Text>
@@ -16,18 +16,13 @@ const TransactionDetail = ({ route }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    padding: 16,
     justifyContent: 'center',
     alignItems: 'center',
-    padding: 16,
-  },
-  header: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    marginBottom: 20,
   },
   text: {
     fontSize: 18,
-    marginVertical: 5,
+    marginBottom: 10,
   },
 });
 
